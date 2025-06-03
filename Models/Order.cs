cfcac687f57874a4beedf67b8e7821e97431a00c
dev <dev@DESKTOP-G6DB6VO>
2025-06-03 19:24:10 +0500

    Added migration and updated database

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,17 +1,25 @@
-﻿namespace ClothingWholesaleAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingWholesaleAPI.Models
 {
     public class Order
     {
         public int Id { get; set; }
-        public required string CustomerName { get; set; } // required qo‘shildi
-        public required string Status { get; set; } // required qo‘shildi
+
+        
+        public  string CustomerName { get; set; } // required qo‘shildi
+        
+        public string Status { get; set; } // required qo‘shildi
         public DateTime OrderDate { get; set; }
         public DateTime? DeliveryDate { get; set; }
         public decimal TotalAmount { get; set; }
         public string Currency { get; set; } = "USD"; // Default qiymat
-        public required string ShippingAddress { get; set; } // required qo‘shildi
-        public required string ContactPhone { get; set; } // required qo‘shildi
-        public required string TrackingNumber { get; set; } // required qo‘shildi
+        
+        public string ShippingAddress { get; set; } // required qo‘shildi
+        
+        public string ContactPhone { get; set; } // required qo‘shildi
+        
+        public string TrackingNumber { get; set; } // required qo‘shildi
         public string? Notes { get; set; } // Nullable qilindi
     }
 }
